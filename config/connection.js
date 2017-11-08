508c72a7df3c8814733f7009b44ae417f927b0a8
Zoli <zjsaunt@gmail.com>
2017-11-08 18:48:55 -0500

    Getting there

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-// <<<<<<< HEAD
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "pyp_db"
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 8889,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "pyp_db"
 // =======
 //   socketPath:"/Applications/MAMP/tmp/mysql/mysql.sock", 
 
