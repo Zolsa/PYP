86767263f85ae5139bdac039bdcc9f84d391c5d1
Zoli <zjsaunt@gmail.com>
2017-11-11 09:51:25 -0500

    heroku?

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,18 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
+//if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "v0cmw26efxm2ieso",
-    password: "so9r26i2kusxn119",
-    database: "y5bnqj40ro5zt1js"
+// } else {
+//   connection = mysql.createConnection({
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "pyp_db"
 
-	});
-}
+// 	});
+// }
 
 connection.connect(function(err) {
   if (err) {
