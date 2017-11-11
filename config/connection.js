c6a5ec5383dbf2fc8d78927ac4e314ee4f75ced7
Zoli <zjsaunt@gmail.com>
2017-11-11 12:11:17 -0500

    heroku attempt 5

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
-
+// Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
-
-//if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 // } else {
-//   connection = mysql.createConnection({
+//   var connection = mysql.createConnection({
+//   // <<<<<<< HEAD
 //     port: 3306,
 //     host: "localhost",
 //     user: "root",
-//     password: "root",
+//     password: "joguktong1!",
 //     database: "pyp_db"
+  // =======
+  //   socketPath:"/Applications/MAMP/tmp/mysql/mysql.sock", 
 
-// 	});
-// }
+  //   user: 'root',
+  //   password: 'root',
+  //   database: "drinks_db"
+  // >>>>>>> jason
+  // });
+// };
 
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -24,4 +30,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
